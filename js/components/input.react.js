49207ed0817faf953a95842371e49b82e00fe49c
tomer kohn <tomokohn@gmail.com>
2016-07-05 00:12:31 +0300

    to form

--- js/components/input.react.js
+++ js/components/input.react.js
@@ -9,29 +9,56 @@ class Input extends React.Component {
 			emailInput: '',
 			massageInput: ''
 			} ;
-		this.restForm = this.restForm.bind(this);
+		this.valueChangeEmail = this.valueChangeEmail.bind(this);
+		this.valueChangeMassage = this.valueChangeMassage.bind(this);
+		this.submitForm = this.submitForm.bind(this);
 	}
-	restForm(){
-		console.log('11');
+
+	valueChangeEmail(e){
+		this.setState({
+			emailInput: e.target.value
+		})
+	}
+
+	valueChangeMassage(e){
+		this.setState({
+			massageInput: e.target.value
+		})
+	}
+
+
+	submitForm(e) {
+		e.preventDefault();
+		Store.dispatch({
+			type: 'ADD_COMMENT',
+			id: this.state.id++,
+			email: this.state.emailInput.value,
+			massage: this.state.massageInput.value
+		});
+		this.setState({
+			emailInput: '',
+			massageInput: ''
+		})
 	}
 
 
 	render() {
 		return (
 			<div className='input'>
-				<input type="email" placeholder="Email"  ref={node => {
-				this.state.emailInput = node;}
-				}/>
-				<textarea placeholder="Massage" ref={node => {
-				this.state.massageInput = node;}
-				}/>
-				<button onClick={() => {Store.dispatch({
-					type: 'ADD_COMMENT',
-					id: this.state.id++,
-					email: this.state.emailInput.value,
-					massage: this.state.massageInput.value
-				});
-				this.restForm}}>+</button>
+				<form action="" onSubmit={()=>this.submitForm(e)}>
+					<input type="email"
+						   placeholder="Email"
+						   required
+						   value={this.state.emailInput}
+						   onChange={this.valueChangeEmail}/>
+					<textarea type="text"
+							  placeholder="Massage"
+							  required
+							  value={this.state.massageInput}
+							  onChange={this.valueChangeMassage}/>
+					<input type="submit"
+						   value="SUBMIT"/>
+				</form>
 			</div>
 		);
 	}
@@ -46,3 +73,17 @@ export default Input;
 //			<p>{comment.massage}</p>
 //		</li>)}
 //</ul>
+/*
+<input type="email" placeholder="Email"  ref={node => {
+				this.state.emailInput = node;}
+				}/>
+<textarea placeholder="Massage" ref={node => {
+	this.state.massageInput = node;}
+}/>
+<button onClick={() => {Store.dispatch({
+	type: 'ADD_COMMENT',
+	id: this.state.id++,
+	email: this.state.emailInput.value,
+	massage: this.state.massageInput.value
+});
+}}>+</button>*/
