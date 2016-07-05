40580360459163f66da1224e9243160fb2837bf9
tomer kohn <tomokohn@gmail.com>
2016-07-05 23:15:01 +0300

    final

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: './index.js',
-    output: { path: __dirname, filename: 'bundle.js' },
+    output: { path: __dirname, filename: 'bundle.js'},
     module: {
         loaders: [
             {
